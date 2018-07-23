bff13d4f3428155b032725ce16304616c19ecc86
rullyindraa <rullyindraa@gmail.com>
2018-07-23 08:54:52 +0700

    edit port

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const path = require('path');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 const fs = require('fs');
-var creds = '';
+var config = '';
 
 const redis = require('redis');
 var client = '';
-var port = process.env.port || 3000;
 
-app.use(express.static('public'));
+fs.readFile('config.json', 'utf-8', function(err, data) {
+  if (err) throw err;
+  config = JSON.parse(data);
+  client = redis.createClient(config.port, config.host, { no_ready_check: true });
+
+  client.auth(config.password, function (err) {
+    if (err) throw err;
+  });
+  
+  client.on('connect', function() {
+    console.log('Connected to Redis');
+  });
+
+  client.once('ready', function() {
+    client.get('chat_users', function(err, reply) {
+      if(reply) {
+        chatters = JSON.parse(reply);
+      }
+    });
+    client.get('chat_app_messages', function(err, reply) {
+      if(reply) {
+        chat_messages = JSON.parse(reply);
+      }
+    });
+  });
+});
+
+var port = process.env.port || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.urlencoded({
   extended: true
 }));
+
+var chatters = [];
+
+var chat_messages = [];
+
+http.listen(port, function() {
+  console.log('Server listening on ' + port);
+});
 
 app.get('/', function (req, res) {
   res.sendFile('views/index.html', {
@@ -22,10 +59,58 @@ app.get('/', function (req, res) {
   });
 });
 
-http.listen(port, function() {
-  console.log('Server listening on ' + port);
+app.post('/join', function(req, res) {
+  var username = req.body.username;
+  if(chatters.indexOf(username) === -1) {
+    chatters.push(username);
+    client.set('chat_users', JSON.stringify(chatters));
+    res.send({
+      'chatters': chatters,
+      'status': 'OK'
+    });
+  } else {
+    res.send({
+      'status': 'FAILED'
+    });
+  }
 });
 
-var chatter = [];
+app.post('/leave', function(req, res) {
+  var username = req.body.username;
+  chatters.splice(chatters.indexOf(username), 1);
+  client.set('chat_users', JSON.stringify(chatters));
+  res.send({
+    'status': 'OK'
+  });
+});
 
-var chat_messages = [];
+app.post('/send_message', function(req, res) {
+  var username = req.body.username;
+  var message = req.body.message;
+  chat_messages.push({
+    'sender': username,
+    'message': message
+  });
+  client.set('chat_app_messages', JSON.stringify(chat_messages));
+  res.send({
+    'status': 'OK'
+  });
+});
+
+app.get('/get_messages', function(req, res) {
+  res.send(chat_messages);
+});
+
+app.get('/get_chatters', function(req, res) {
+  res.send(chatters);
+});
+
+io.on('connection', function(socket) {
+  socket.on('message', function(data) {
+    io.emit('send', data);
+  });
+
+  socket.on('update_chatter_count', function(data) {
+    io.emit('count_chatters', data);
+  });
+});
